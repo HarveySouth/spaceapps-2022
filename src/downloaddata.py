b2cb88f84330d6b535f5452e093447759a629433
Edmund H <edmundhenley@users.noreply.github.com>
2022-10-02 04:35:19 +0100

    Add notebook demoing pandas usage
    Notebook shows how to read our data into pandas, and
    filtering the resulting data based on CME dates.
    
    Adds links to various useful sources, and explains a little
    more about the data for the team.
    
    GH-7

--- src/downloaddata.py
+++ src/downloaddata.py
@@ -1,4 +1,12 @@
 import spacepy
 import spacepy.toolbox
-spacepy.toolbox.update(all=True)
+
+def download_data():
+    """Use spacepy to update all data sources
+    
+    By default this will add data to ~/.spacepy/data
+    
+    See https://spacepy.github.io/configuration.html
+    """
+    spacepy.toolbox.update(all=True)
 
